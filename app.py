85ae46bba3c9d2c34d410d9733c0acbad59feac0
Imwatchingu60 <you@example.com>
2026-07-30 09:42:38 +1200

    save it again

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app= Flask(__name__)
+app = Flask(__name__, template_folder='.')
+
+FLOWERS={'id':1,'name':'Sunflower','latin':'Helianthus annuus','season':'Summer','sunlight':'Full Sun','watering':'Medium','difficulty': 'Easy'},{'id':2, 'name':'Rose','latin':'Rosa','season':'Spring-Autumn','sunlight':'Full Sun','watering':'Medium','difficulty':'Hard'},{'id':3,'name':'Lavender','latin':'lavare','season':'Summer','sunlight':'Full Sun','watering':'Low','difficulty':'Easy'},
+
 
 @app.route('/')
 def home():
-    #home page
-    return "oh my gosh "
-if __name__=='__main__':
+    return render_template('index.html')
+
+@app.route('/flowers')
+def flowers():
+    return render_template('flowers.html',flowers=FLOWERS)
+
+
+@app.route('/flower/<int:id>')
+def flower_detail(id):
+    flower = None
+    for f in FLOWERS:
+        if f['id'] == id:
+            flower = f
+    return render_template('flower.html', flower=flower)
+
+if __name__ =='__main__':
     app.run(debug=True)
