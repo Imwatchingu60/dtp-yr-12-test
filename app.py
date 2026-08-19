ea20667932ec21e3359f050431507796125c7803
Imwatchingu60 <you@example.com>
2026-08-19 20:10:06 +1200

    fixed

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def get_db():
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
         db.row_factory = sqlite3.Row
-        db.execute("PRAGMA foreign_kets = ON")
+        db.execute("PRAGMA foreign_keys = ON")
     return db
 
 
